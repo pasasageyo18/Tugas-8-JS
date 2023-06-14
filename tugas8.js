b5c5d6ca01c50da16009c7e0593dc50350c0e96e
Muhammad Nur Faza <gofar222@gmail.com>
2023-06-14 13:35:57 +0700

    first commit

---
+++ tugas8.js
@@ -0,0 +1,11 @@
+let motor = {
+  type: "kopling",
+  nama: "JupiterMX",
+  warna: "Merah",
+  harga: 200000000,
+};
+
+console.log(motor);
+motor.warna = "biru";
+motor.harga = 100000;
+console.log(motor);
